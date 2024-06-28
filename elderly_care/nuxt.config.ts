9de75b8398c16e3b21c01084223f07944900ba97
chi030303 <1726634055@qq.com>
2024-06-28 13:27:24 +0800

    前端框架

---
+++ elderly_care/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+  ]
+})
---
+++ elderly_care/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="bg-red-500">
+            TESTING
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
