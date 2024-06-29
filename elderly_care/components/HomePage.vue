e15ce6c5d5ab3fad8d4a3e6c1ba93d46029a4f38
chi030303 <1726634055@qq.com>
2024-06-29 13:48:03 +0800

    Changes to be committed: 	renamed:    elderly_care/components/AddElderlyModal.vue -> elderly_care/components/AddElderlyOverlap.vue 	modified:   elderly_care/components/ElderlyManagement.vue 	modified:   elderly_care/components/HomePage.vue 	modified:   elderly_care/components/SideNav.vue 	new file:   elderly_care/components/TextInput.vue 	modified:   elderly_care/nuxt.config.ts 	modified:   elderly_care/package-lock.json 	modified:   elderly_care/package.json 	renamed:    elderly_care/stores/pageStore.js -> elderly_care/stores/page.js

--- elderly_care/components/HomePage.vue
+++ elderly_care/components/HomePage.vue
@@ -14,7 +14,7 @@ import { ref, computed } from 'vue';
 import ElderlyManagement from '~/components/ElderlyManagement.vue';
 import StaffManagement from '~/components/StaffManagement.vue';
 import VolunteerManagement from '~/components/VolunteerManagement.vue';
-import { usePageStore } from '~/stores/pageStore';
+import { usePageStore } from '~/stores/page';
 
 const store = usePageStore();
 const currentPage = computed(() => store.currentPage);
