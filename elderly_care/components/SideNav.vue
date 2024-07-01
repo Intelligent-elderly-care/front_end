a7aad6a3d25afa30b57c1672bdb4bd2bdb5cc4ec
chi030303 <1726634055@qq.com>
2024-07-01 20:52:12 +0800

    完善剩余功能，美化UI

--- elderly_care/components/SideNav.vue
+++ elderly_care/components/SideNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex min-h-screen">
-        <div :class="isOpen ? 'w-40' : 'w-60'" class="flex flex-col min-h-screen p-3 duration-300 bg-gray-800 shadow">
+        <div :class="isOpen ? 'w-50' : 'w-50'" class="flex flex-col min-h-screen p-3 duration-300 bg-gray-800 shadow">
             <div class="space-y-3">
                 <div class="flex items-center justify-between">
                     <h2 class="text-xl font-bold text-white">智慧养老平台</h2>
@@ -9,16 +9,6 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                     </button>
-                </div>
-                <div class="relative">
-                    <span class="absolute inset-y-0 left-0 flex items-center py-4">
-                        <button type="submit" class="p-2 focus:outline-none focus:ring">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                            </svg>
-                        </button>
-                    </span>
-                    <input type="search" name="Search" placeholder="搜索..." class="w-full py-2 pl-10 text-sm rounded-md focus:outline-none" />
                 </div>
                 <div class="flex-1">
                     <ul class="pt-2 pb-4 space-y-1 text-sm">
@@ -34,25 +24,25 @@
                             </button>
                             <ul v-show="toggleManageMenu" class="pl-4 pt-2 pb-1 text-sm">
                                 <li>
-                                    <button @click="setPage('elderly')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-3 block w-full text-left rounded-md">
+                                    <button @click="setPage('elderly'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-3 block w-full text-left rounded-md">
                                         <Icon name="material-symbols:elderly" class="text-gray-100 mr-2" size="35"/>
                                         老人信息管理
                                     </button>
                                 </li>
                                 <li>
-                                    <button @click="setPage('staff')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
+                                    <button @click="setPage('staff'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
                                         <Icon name="guidance:care-staff-area" class="text-gray-100 mr-2" size="30"/>
                                         工作人员信息管理
                                     </button>
                                 </li>
                                 <li>
-                                    <button @click="setPage('volunteer')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
+                                    <button @click="setPage('volunteer'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
                                         <Icon name="material-symbols:volunteer-activism" class="text-gray-100 mr-2" size="30"/>
                                         义工信息管理
                                     </button>
                                 </li>
                                 <li>
-                                    <button @click="setPage('event')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
+                                    <button @click="setPage('event'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
                                         <Icon name="material-symbols:volunteer-activism" class="text-gray-100 mr-2" size="30"/>
                                         事件管理
                                     </button>
@@ -60,30 +50,30 @@
                             </ul>
                         </li>
                         <li class="rounded-sm">
-                            <button @click="toggleManageMenu = !toggleManageMenu" class="flex items-center justify-between w-full p-2 space-x-3 rounded-md hover:bg-gray-600 cursor-pointer">
+                            <button @click="toggleCameraMenu = !toggleCameraMenu" class="flex items-center justify-between w-full p-2 space-x-3 rounded-md hover:bg-gray-600 cursor-pointer">
                                 <div class="flex items-center space-x-3">
                                     <Icon name="mage:camera" class="text-gray-100" size="30"/>
                                     <span class="text-gray-100">实时视频检测</span>
                                 </div>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-100 transform transition-transform" :class="{ 'rotate-180': toggleManageMenu }">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-100 transform transition-transform" :class="{ 'rotate-180': toggleCameraMenu }">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"/>
                                 </svg>
                             </button>
-                            <ul v-show="toggleManageMenu" class="pl-4 pt-2 pb-1 text-sm">
+                            <ul v-show="toggleCameraMenu" class="pl-4 pt-2 pb-1 text-sm">
                                 <li>
-                                    <button @click="setPage('face')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-3 block w-full text-left rounded-md">
+                                    <button @click="setPage('face'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-3 block w-full text-left rounded-md">
                                         <Icon name="material-symbols:elderly" class="text-gray-100 mr-2" size="35"/>
                                         人脸识别
                                     </button>
                                 </li>
                                 <li>
-                                    <button @click="setPage('emotion')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
+                                    <button @click="setPage('emotion'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
                                         <Icon name="guidance:care-staff-area" class="text-gray-100 mr-2" size="30"/>
                                         情绪识别
                                     </button>
                                 </li>
                                 <li>
-                                    <button @click="setPage('fallOver')" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
+                                    <button @click="setPage('fallOver'); closeAllMenus()" class="text-gray-100 hover:text-white hover:bg-gray-600 cursor-pointer py-2 px-4 block w-full text-left rounded-md">
                                         <Icon name="material-symbols:volunteer-activism" class="text-gray-100 mr-2" size="30"/>
                                         摔倒检测
                                     </button>
@@ -91,7 +81,7 @@
                             </ul>
                         </li>
                         <li class="rounded-sm">
-                            <button @click="setPage('settings')" class="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-600 cursor-pointer w-full">
+                            <button @click="setPage('settings'); closeAllMenus()" class="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-600 cursor-pointer w-full">
                                 <Icon name="mage:settings" class="text-gray-100" size="30"/>
                                 <span class="text-gray-100">系统设置</span>
                             </button>
@@ -109,9 +99,15 @@ import { usePageStore } from '~/stores/page';
 
 const isOpen = ref(false);
 const toggleManageMenu = ref(false);
+const toggleCameraMenu = ref(false);
 const store = usePageStore();
 
 const setPage = (page) => {
     store.setPage(page);
+};
+
+const closeAllMenus = () => {
+    toggleManageMenu.value = false;
+    toggleCameraMenu.value = false;
 };
 </script>
--- elderly_care/components/TopNav.vue
+++ elderly_care/components/TopNav.vue
@@ -1,30 +1,59 @@
 <template>
-    <header class="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <h1 class="text-xl">智能养老系统</h1>
-      <div class="flex items-center space-x-4">
-        <div class="flex items-center space-x-2">
-          <img src="https://picsum.photos/id/83/300/320" alt="User Avatar" class="w-8 h-8 rounded-full">
-          <span>用户名</span>
-        </div>
-        <div class="flex flex-col items-center">
-          <span>今天的天气：晴</span>
-          <span>日期：2024-06-28</span>
-          <span>时间：14:30</span>
-        </div>
-        <div class="rounded-sm">
-          <a href="#" class="flex items-center p-2 space-x-3 rounded-md">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-100">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-            </svg>
-            <span class="text-gray-100"> Logout </span>
-          </a>
-        </div>
+  <header class="bg-gray-700 text-white p-4 flex items-center">
+    <h1 class="text-xl flex-grow text-center pl-40">智能养老系统</h1>
+    <div class="flex items-center space-x-8">
+      <div class="flex flex-col items-center">
+        <span>日期：{{ date }}</span>
+        <span>时间：{{ time }}</span>
       </div>
-    </header>
-  </template>
-  
-  <script setup>
-  </script>
-  
+    </div>
+
+    <!-- 空白元素 -->
+    <div style="width: 20px;"></div>
+
+    <div class="flex items-center space-x-2">
+        <img src="https://picsum.photos/id/83/300/320" alt="User Avatar" class="w-8 h-8 rounded-full cursor-pointer" @click="toggleDropdown">
+        <span>{{ username }}</span>
+    </div>
+    <div v-if="dropdownVisible" class="absolute right-4 top-16 bg-white text-black rounded-lg shadow-lg p-4">
+      <p>用户信息</p>
+      <hr class="my-2">
+      <a href="#" @click="logout">注销</a>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useGeneralStore } from '~/stores/general';
+
+const store = useGeneralStore();
+
+const date = ref('');
+const time = ref('');
+const dropdownVisible = ref(false);
+const username = computed(() => store.name);
+const router = useRouter();
+
+const toggleDropdown = () => {
+  dropdownVisible.value = !dropdownVisible.value;
+};
+
+const fetchDateTime = () => {
+  const now = new Date();
+  date.value = now.toLocaleDateString();
+  time.value = now.toLocaleTimeString();
+};
+
+onMounted(() => {
+  fetchDateTime();
+  setInterval(fetchDateTime, 600); // 每秒钟更新一次时间
+});
+
+const logout = () => {
+  // 处理注销逻辑
+  store.logout();
+  router.push('/');
+  console.log('用户已注销');
+};
+</script>
